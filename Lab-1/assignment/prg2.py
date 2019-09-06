14ae7460e98eb6512ad613e9b8018490054fa0bc
Adarsh Biradar <43452317+adarshbiradar@users.noreply.github.com>
2019-09-06 07:08:22 +0530

    Add files via upload

---
+++ Lab-1/assignment/prg2.py
@@ -0,0 +1,10 @@
+# creating a tuple with numbers
+
+tup = (1,2,3)
+
+# print one item of the tuple
+print("First Item of the Tuple : ",tup[0])
+
+# Convert this tuple to a list
+lst=list(tup)
+print(lst)
